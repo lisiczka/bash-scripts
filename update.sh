1fa50b2b6d1bf0b8438c359e4d79b42a20e09a74
lis <michuge@gmail.com>
2020-01-28 16:51:43 +0100

    little update

--- update.sh
+++ update.sh
@@ -3,11 +3,11 @@
 # Bash script that'll keep your linux (Ubuntu) up-to-date.
 
 echo UPDATE
-sudo apt-get update
-echo 
+echo y | sudo apt-get update
+echo
 echo UPGRADE
 echo y | sudo apt-get upgrade
 echo
 echo CLEANUP
-sudo apt-get autoclean
-sudo apt-get autoremove
+echo y | sudo apt-get autoclean
+echo y | sudo apt-get autoremove
